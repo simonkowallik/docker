75d20bc7e4c50dc945ffc69757b05568ab8a7b63
Simon Kowallik <github@simonkowallik.com>
2019-03-26 20:08:49 +0100

    updates travis ci scripts

--- _ci/run-tests.sh
+++ _ci/run-tests.sh
@@ -3,5 +3,8 @@
 set -ev
 for docker_image in $(git show --format='' --name-only HEAD | cut -d'/' -f1 | uniq)
 do
-  ./${docker_image}/tests/run-tests.sh $docker_image
+  if [[ -d $docker_image ]] && [[ -x ./${docker_image}/tests/run-tests.sh ]]
+  then
+    ./${docker_image}/tests/run-tests.sh $docker_image
+  fi
 done
