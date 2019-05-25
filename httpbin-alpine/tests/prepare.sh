464146d8ead58c4387003d186c59d0ee51f384ca
Simon Kowallik <github@simonkowallik.com>
2019-05-25 13:11:39 +0200

    httpbin-alpine: fix tests

--- httpbin-alpine/tests/prepare.sh
+++ httpbin-alpine/tests/prepare.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -ev
 docker build -t httpbin-alpine-ci-tests $1
-docker run --rm -d -p 8080:80 --name httpbin-alpine-ci-tests httpbin-alpine-ci-tests
+docker run --rm -d -p 8000:80 --name httpbin-alpine-ci-tests httpbin-alpine-ci-tests
